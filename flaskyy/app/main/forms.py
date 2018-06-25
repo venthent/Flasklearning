9bcf363deed13e59ea9669240bf1b88dc7fc823c
Vincent <1627237372@qq.com>
2018-06-25 16:59:34 +0800

    Chapter 10.3

--- flaskyy/app/main/forms.py
+++ flaskyy/app/main/forms.py
@@ -1,7 +1,50 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField,StringField
-from wtforms.validators import DataRequired
+from wtforms import SubmitField, StringField, TextAreaField, BooleanField, SelectField, ValidationError
+from wtforms.validators import DataRequired, Length, EqualTo, Email, Regexp
+from Flasklearning.flaskyy.app.models import Role, User
+
 
 class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+
+class EditProfileForm(FlaskForm):
+    name = StringField("Real Name:", validators=[DataRequired(), Length(0, 64)])
+    location = StringField("Location", validators=[Length(0, 64)])
+    about_me = TextAreaField("About me")
+    submit = SubmitField("Submit")
+
+
+class EditProfileAdminForm(FlaskForm):  # 管理员使用的资料编辑表单
+    name = StringField("Real Name:", validators=[Length(0, 64)])
+    location = StringField("Location", validators=[Length(0, 64)])
+    about_me = TextAreaField("About me")
+
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(0, 64)])
+    username = StringField('Username', validators=[DataRequired(), Length(0, 64),
+                                                   Regexp('^[A-Za-z][A-Aa-z0-9._]*$', flags=0,
+                                                          message='Usernames must have only letters, numbers, dots'
+                                                                  'or underscores')])
+    confirmed = BooleanField("Confirmed")
+    role = SelectField('Role', coerce=int)  # coerce=int 参数,从而把字段的值转换为整数
+    submit = SubmitField("Submit")
+
+    def __init__(self, user, *args, **kwargs):  # Here is difficult to understand
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name) for role in Role.query.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self, field):
+        '''如 果 表 单 类 中 定 义 了 以
+        validate_ 开头且后面跟着字段名的方法,这个方法就和常规的验证函数一起调用,just is 自定义的验证方法.
+mail 和 username 字段的构造方式和认证表单中的一样,但处理验证时需要更加小心。验
+证这两个字段时,首先要检查字段的值是否发生了变化,如果有变化,就要保证新值不
+和其他用户的相应字段值重复;如果字段值没有变化,则应该跳过验证。为了实现这个逻辑,表单构造函数接收用户对象作为参数,并将其保存在成员变量中,随后自定义的验证
+方法要使用这个用户对象'''
+        if field.data != self.user.email and User.query.filter_by(email=field.data).first():
+            raise ValidationError("Email already registered ")
+
+    def validate_username(self, field):
+        if field.data != self.user.username and User.query.filter_by(username=field.data).first():
+            raise ValidationError("Username already in use")
--- flaskyy/app/main/views.py
+++ flaskyy/app/main/views.py
@@ -1,13 +1,15 @@
 from datetime import datetime
-from flask import render_template,session,redirect,url_for,abort
+from flask import render_template, session, redirect, url_for, abort, flash
 from Flasklearning.flaskyy.app.main import main
-from .forms import NameForm
-from Flasklearning.flaskyy.app import db,models
+from .forms import NameForm, EditProfileForm, EditProfileAdminForm
+from Flasklearning.flaskyy.app import db, models
+from flask_login import login_required, current_user
+from Flasklearning.flaskyy.app.decorators import admin_required
 
 
-@main.route('/',methods=['GET', 'POST'])
+@main.route('/', methods=['GET', 'POST'])
 def index():
-    form=NameForm()
+    form = NameForm()
     if form.validate_on_submit():
         user = models.User.query.filter_by(username=form.name.data).first()
         old_name = session.get('name')
@@ -20,16 +22,62 @@ def index():
             session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
-        #蓝本中可以省略蓝本名,例如 url_for('.
-        #index') 。在这种写法中,命名空间是当前请求所在的蓝本
+        # 蓝本中可以省略蓝本名,例如 url_for('.
+        # index') 。在这种写法中,命名空间是当前请求所在的蓝本
         return redirect(url_for('.index'))
     return render_template('index.html', name=session.get('name'), form=form, known=session.get('known', False))
 
 
 @main.route('/user/<username>')
 def user(username):
-    user=models.User.query.filter_by(username=username).first()
+    user = models.User.query.filter_by(username=username).first()
     if user is None:
         abort(404)
     else:
-        return render_template('user.html',user=user)
+        return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=["GET", "POST"])  # 用户级别的资料编辑器
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        db.session.commit()
+        flash(message='Your profile has been updated')
+        return redirect(url_for('main.user', username=current_user.username))
+    # 当 form.validate_on_submit()返回 False 时,表单中的 3 个字段都使用 current_user 中保存的初始值
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])  # 管理员的资料编辑路由
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = models.User.query.get_or_404(id)  # Like get() but aborts with 404 if not found instead of returning None.
+    form = EditProfileAdminForm(user=user)  # 传入 user
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = models.Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        db.session.commit()
+        flash('The profile has been updated.')
+        return redirect(url_for('main.user',username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data =user.confirmed
+    form.name.data =user.name
+    form.location.data =user.location
+    form.about_me.data =user.about_me
+    return render_template('edit_profile.html',form=form,user=user)
